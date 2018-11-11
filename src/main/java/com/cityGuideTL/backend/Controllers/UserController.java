52e09407b8c3feea8b2207902d0c344464052b30
greycr0w <alexbant@teiser.gr>
2018-11-11 15:20:49 +0200

    User model changes complete, with backed up UserController functionality

--- src/main/java/com/cityGuideTL/backend/Controllers/UserController.java
+++ src/main/java/com/cityGuideTL/backend/Controllers/UserController.java
@@ -12,26 +12,43 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserRepository userRepository;
+    //get all users
+    @GetMapping(path="/user")
+    @ResponseBody
+    public List<User> getAll() {
+        List<User> list = userRepository.findAll();
+        return list;
+    }
 
-    @PostMapping(path="/add")
-    public @ResponseBody
-    User addUser(@RequestBody User body) {
+    //create a user
+    @PostMapping(path="/user")
+    @ResponseBody
+    public void addUser(@RequestBody User body) {
         User user = new User();
         user.setPassword(body.getPassword());
         userRepository.save(body);
-        return body;
     }
 
-    @PostMapping(path="/delete")
-    public @ResponseBody
-    void deleteUser(@RequestParam Integer id) {
+    //get user with specified id
+    @GetMapping(path="/user/{id}")
+    @ResponseBody
+    public User getUser(@RequestParam Integer id) {
+        User user = userRepository.findById(id).get();
+        return user;
+    }
+
+    //delete a user with specified id
+    @DeleteMapping(path="/user/{id}")
+    @ResponseBody
+    public void deleteUser(@RequestParam Integer id) {
         User user = userRepository.findById(id).get();
         userRepository.delete(user);
     }
 
-    @GetMapping(path="/all")
-    public List<User> getAll() {
-        List<User> list = userRepository.findAll();
-        return list;
+    @PutMapping(path="/user/{id}")
+    @ResponseBody
+    public void updateUser(@RequestBody User body) {
+        User user = body;
+        userRepository.save(user);
     }
 }
