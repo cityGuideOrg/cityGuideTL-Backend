f92e265fc41fb0835a47fe2e7c94b8a2ca267c50
IgorSpi <igor13pao@hotmail.com>
2018-11-11 23:20:27 +0200

    Remove useless code

--- src/main/java/com/cityGuideTL/backend/Controllers/FlickrController.java
+++ src/main/java/com/cityGuideTL/backend/Controllers/FlickrController.java
@@ -2,23 +2,11 @@ package com.cityGuideTL.backend.Controllers;
 
 import com.cityGuideTL.backend.Entities.City;
 import com.cityGuideTL.backend.Services.FlickrService;
-import com.flickr4java.flickr.Flickr;
-import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.photos.Photo;
-import com.flickr4java.flickr.photos.PhotoList;
-import com.flickr4java.flickr.photos.SearchParameters;
-import com.flickr4java.flickr.places.Place;
-import com.flickr4java.flickr.places.PlacesList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/flickr")
@@ -26,54 +14,6 @@ public class FlickrController {
 
     @Autowired
     private FlickrService flickrService;
-
-    @RequestMapping(value = "/search/{city}", method = RequestMethod.GET)
-    public List<Place> search(@PathVariable("city") String city) {
-
-        Flickr f = new Flickr("e6ca2e5d10eb5160e389625f9bfa5439", "", new REST());
-try {
-    PlacesList<Place> placesList = f.getPlacesInterface().find(city);
-    Place place = placesList.get(0);
-
-    SearchParameters params = new SearchParameters();
-
-    params.setPlaceId(place.getPlaceId());
-    params.setHasGeo(true);
-    Set<String> extras = new HashSet<String>();
-    extras.add("geo");
-    extras.add("tags");
-    params.setExtras(extras);
-
-    PhotoList<Photo> photos = f.getPhotosInterface().search(params, 100, 1);
-
-    for(int i = 0; i < photos.size(); ++i) {
-        Photo photo = photos.get(i);
-
-        photo.getGeoData().getLatitude();
-        photo.getGeoData().getLatitude();
-
-
-    }
-
-
-
-    PlacesList<Place> interesting = f.getPlacesInterface().getTopPlacesList(7, null, place.getPlaceId(), null);
-
-    List<Place> places = new LinkedList<Place>();
-
-    for (int i = 0; i < Math.min(5, interesting.size()); ++i) {
-        ((LinkedList<Place>) places).addLast(interesting.get(i));
-    }
-
-    return places;
-
-} catch (Exception e) {
- System.out.println(e.getMessage());
-}
-return null;
-
-
-    }
 
     @RequestMapping(value = "/{city}", method = RequestMethod.GET)
     public City getMostVisitedPlaces(@PathVariable("city") String city){
