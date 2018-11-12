341b8767924aa2a2ad4e8672d41c6812aaf96599
greycr0w <alexbant@teiser.gr>
2018-11-12 22:24:07 +0200

    Fixing request methods to convencional correct ones & catching exceptions and handling them on requests

--- src/main/java/com/cityGuideTL/backend/Controllers/UserController.java
+++ src/main/java/com/cityGuideTL/backend/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.cityGuideTL.backend.Controllers;
 
 import com.cityGuideTL.backend.Models.UserModel;
 import com.cityGuideTL.backend.Repository.UserRepository;
+import com.cityGuideTL.backend.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,55 +12,37 @@ import java.util.List;
 @RequestMapping(path="/user")
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+    private UserService userService = new UserService();
     //get all users
     @GetMapping
     @ResponseBody
     public List<UserModel> getAll() {
-        List<UserModel> list = userRepository.findAll();
-        return userRepository.findAll();
+        return userService.getAll();
     }
 
     //create a user
-    @PostMapping
-    @ResponseBody
     public void addUser(@RequestBody UserModel body) {
-        UserModel userModel = new UserModel();
-        userModel.setPassword(body.getPassword());
-        userRepository.save(body);
+       userService.addUser(body);
     }
 
     //get user with specified id
     @GetMapping(path="/{id}")
     @ResponseBody
     public UserModel getUser(@PathVariable Integer id) {
-        UserModel userModel = userRepository.findById(id).get();
-        return userModel;
+        return userService.getUser(id);
     }
 
     //delete a user with specified id
     @DeleteMapping(path="/{id}")
     @ResponseBody
     public void deleteUser(@PathVariable Integer id) {
-        UserModel userModel = userRepository.findById(id).get();
-        userRepository.delete(userModel);
+        userService.deleteUser(id);
     }
 
     @PutMapping(path="/{id}")
     @ResponseBody
     public void updateUser(@RequestBody UserModel body, @PathVariable Integer id) {
-        //you cant simply replace the object because the null id is replaced also
-        //you either need to get an id on the body and replace the whole user and save it or just replace the names etc.
-        UserModel userModelToUpdate = userRepository.getOne(id);
-        userModelToUpdate.setFirstname(body.getFirstname());
-        if(body.getPassword()!=null) {
-            //validate on UserModel entity
-        }
-        userModelToUpdate.setLastname(body.getLastname());
-        userModelToUpdate.setEmail(body.getEmail());
-        userModelToUpdate.setPassword(body.getPassword());
-        userModelToUpdate.setUsername(body.getUsername());
-        userRepository.save(body);
-        //wait for best practice
+        userService.updateUser(body, id);
     }
 }
--- src/main/java/com/cityGuideTL/backend/Services/UserService.java
+++ src/main/java/com/cityGuideTL/backend/Services/UserService.java
@@ -1,10 +1,65 @@
 package com.cityGuideTL.backend.Services;
 
+import com.cityGuideTL.backend.Models.UserModel;
 import com.cityGuideTL.backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 //todo: access jpaapi through this service, commit failcheck code
+//todo: map entities to models
 public class UserService {
     @Autowired
-   private UserRepository userRepository;
+    UserRepository userRepository;
+    //get all users
+
+    public List<UserModel> getAll() {
+        List<UserModel> list = userRepository.findAll();
+        return userRepository.findAll();
+    }
+
+    //create a user
+
+    public void addUser(UserModel body) {
+        UserModel userModel = new UserModel();
+        userModel.setPassword(body.getPassword());
+        userRepository.save(body);
+    }
+
+    //get user with specified id
+    public UserModel getUser(Integer id) {
+        UserModel userModel = userRepository.findById(id).get();
+        return userModel;
+    }
+
+    //delete a user with specified id
+    public void deleteUser(Integer id) {
+        UserModel userModel;
+        try {
+            userModel = userRepository.findById(id).orElseThrow(IOException::new);
+        }
+        catch (IOException e) {
+            System.out.println("User not found");
+            userModel = null;
+        }
+        userRepository.delete(userModel);
+    }
+
+    public void updateUser(UserModel body, Integer id) {
+        //you cant simply replace the object because the null id is replaced also
+        //you either need to get an id on the body and replace the whole user and save it or just replace the names etc.
+        UserModel userModelToUpdate = userRepository.getOne(id);
+        userModelToUpdate.setFirstname(body.getFirstname());
+        if(body.getPassword()!=null) {
+            //validate on UserModel entity
+        }
+        userModelToUpdate.setLastname(body.getLastname());
+        userModelToUpdate.setEmail(body.getEmail());
+        userModelToUpdate.setPassword(body.getPassword());
+        userModelToUpdate.setUsername(body.getUsername());
+        userRepository.save(body);
+        //wait for best practice
+    }
 
 }
